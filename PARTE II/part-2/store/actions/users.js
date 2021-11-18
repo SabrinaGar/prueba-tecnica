696fbda12c19bb9219cc593dab8f838796ce41d1
SabrinaGar <sabrina.garcia@senkosoft.com>
2021-11-18 01:23:29 -0400

    Part II store 2

---
+++ PARTE II/part-2/store/actions/users.js
@@ -0,0 +1,42 @@
+import * as t from "../types";
+
+export const setModalOpen = (isModalOpen) => {
+	return {
+		type: t.MODAL_OPEN,
+		payload: isModalOpen,
+	};
+};
+
+export const fetchUsers = () => {
+	return {
+		type: t.USER_FETCH_REQUESTED,
+	};
+};
+
+export const addUser = (user) => {
+	return {
+		type: t.USER_ADD_REQUESTED,
+		payload: user,
+	};
+};
+
+export const updateUser = (user) => {
+	return {
+		type: t.USER_UPDATE_REQUESTED,
+		payload: user,
+	};
+};
+
+export const deleteUser = (id) => {
+	return {
+		type: t.USER_DELETE_REQUESTED,
+		payload: id,
+	};
+};
+
+export const setSelectedUser= (id) => {
+	return {
+		type: t.USER_SELECTED,
+		payload: id,
+	};
+};
